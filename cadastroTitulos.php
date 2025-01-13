dc52802e5e4f39d4a3eab657f34a56def163cb9e
Matheus de Oliveira Timotheo <111666891+MatheusFireRed@users.noreply.github.com>
2025-01-13 14:09:04 -0300

    Adicionados novos inputs para cadastro de titulos
    Foram adicionados inputs no formulário HTMLpara capturar mais informações sobre os títulos.

--- cadastroTitulos.php
+++ cadastroTitulos.php
@@ -9,12 +9,16 @@ if (!isset($_SESSION['user_id'])) {
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Pragma: no-cache");
 header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
+
+
+$tituloDaPagina = 'Area de Criação';
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Área Restrita</title>
+    <title><?php echo $tituloDaPagina ?></title>
 
     <link rel="stylesheet" href="style/styleCadastroTitulo.css">
 </head>
@@ -23,16 +27,16 @@ header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
     <header>
 
         <div class="box-saudacao">
-            <h1>Bem-vindo, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+            <h1 class="saudacao">Bem-vindo, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
         </div>
 
         <div class="box-menu">
             <ul class="menu">
                <li>
-                    <a href="cadastroTitulos.php">Cadastrar Titulo</a>
+                    <a href="cadastroTitulos.php">Cadastrar <br> Titulo</a>
                </li> 
                <li>
-                    <a href="#">Comentar sobre titulo</a>
+                    <a href="#">Comentar <br> sobre <br> titulo</a>
                </li>
             </ul>
         </div>
@@ -44,36 +48,83 @@ header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
     <main>
 
         <div class="card">
+
             <form action="php/DB/incluirTitulo.php" method="POST">
-                <div class="label-input">
-                    <label for="titulo">Título</label>
-                    <input type="text" name="titulo" id="titulo">
+
+                <div class="coluna">
+                    <div class="label-input">
+                        <label for="titulo">Título</label>
+                        <input type="text" name="titulo" id="titulo">
+                    </div>
+
+                    <div class="label-input">
+                        <label for="titulo-original">Titulo Original</label>
+                        <input type="text" name="titulo-original" id="titulo-original">
+                    </div>
+
+                    <div class="label-input">
+                        <label for="tipo">Tipo</label>
+                        <select name="tipo" id="tipo">
+                            <option value="">Selecionar..</option>
+                            <option value="serie">Série</option>
+                            <option value="filme">Filme</option>
+                            <option value="anime">Anime</option>
+                            <option value="documentario">Documentário</option>
+                            <option value="reality-show">Reality Show</option>
+                        </select>
+                    </div>
+
+                    <div class="label-input">
+                        <label for="categoria">Categoria</label>
+                        <select name="categoria" id="categoria">
+                            <option value="">Selecionar..</option>
+                            <option value="terror">Terror</option>
+                            <option value="comedia">Comédia</option>
+                            <option value="acao">Ação</option>
+                            <option value="romance">Romance</option>
+                        </select>
+                    </div>
+
+                    <div class="label-input">
+                        <label for="diretor">Diretor</label>
+                        <input type="text" id="diretor" name="diretor">
+                    </div>
+
+                    <div class="label-input">
+                        <label for="durecao">Duração</label>
+                        <input type="text" id="duracao" name="durecao">
+                    </div>
+                    
+                    <div class="label-input">
+                        <label for="data-estreia">Data da estréia</label>
+                        <input type="date" name="data-estreia" id="data-estreia">
+                    </div>
+
+                    <div class="label-input">
+                        <label for="pais-origem">País de Origem</label>
+                        <input type="text" id="pais-origem" name="pais-origem">
+                    </div>
+
+                    <div class="label-input">
+                        <label for="roteiro">Roteiro</label>
+                        <input type="text" name="roteiro" id="roteiro">
+                    </div>
                 </div>
 
-                <div class="label-input">
-                    <label for="tipo">Tipo</label>
-                    <select name="tipo" id="tipo">
-                        <option value="">Selecionar..</option>
-                        <option value="serie">Série</option>
-                        <option value="filme">Filme</option>
-                        <option value="anime">Anime</option>
-                        <option value="documentario">Documentário</option>
-                    </select>
-                </div>
+                <div class="coluna">
+                    <div class="label-input">
+                        <label for="meta-dados">MetaDados</label>
+                        <textarea name="meta-dados" id="meta-dados"></textarea>
+                    </div>
 
-                <div class="label-input">
-                    <label for="categoria">Categoria</label>
-                    <select name="categoria" id="categoria">
-                        <option value="">Selecionar..</option>
-                        <option value="terror">Terror</option>
-                        <option value="comedia">Comédia</option>
-                        <option value="acao">Ação</option>
-                        <option value="romance">Romance</option>
-                    </select>
+                    <div class="label-input">
+                        <label for="sinopse">Sinopse</label>
+                        <textarea name="sinopse" id="sinopse"></textarea>
+                    </div>
                 </div>
-
                 <button type="submit">Enviar</button>
             </form>
+                
         </div>
 
     </main>
--- php/DB/incluirTitulo.php
+++ php/DB/incluirTitulo.php
@@ -1,16 +1,57 @@
 <?php
     require_once 'conexaoDB.php';
 
-    $titulo =  $_POST['titulo'];
-    $tipo   =  $_POST['tipo'];
-    $categoria = $_POST['categoria'];
+    $titulo             = $_POST['titulo'];
+    $tipo               = $_POST['tipo'];
+    $categoria          = $_POST['categoria'];
+    $diretor            = $_POST['diretor'];
+    $duracao            = $_POST['duracao'];
+    $tituloOriginal     = $_POST['titulo-original'];
+    $dataEstreia        = $_POST['data-estreia'];
+    $metaDados          = $_POST['meta-dados'];
+    $sinopse            = $_POST['sinopse'];
+    $paisOrigem         = $_POST['pais-origem'];
+    $roteiro            = $_POST['roteiro'];
 
     try{
-        $sql = "INSERT INTO titulos (titulo, tipo, categoria) VALUES (:titulo, :tipo, :categoria)";
+        $sql = "INSERT INTO titulos (titulo,
+         tipo,
+         categoria, 
+         diretor,
+         duracao,
+         titulo_original,
+         data_estreia,
+         meta_dados,
+         sinopse,
+         pais_origem,
+         roteiro) 
+         VALUES
+         (:titulo,
+          :tipo,
+          :categoria,
+          :diretor,
+          :duracao,
+          :titulo_original,
+          :data_estreia,
+          :meta_dados,
+          :sinopse,
+          :pais_origem,
+          :roteiro)";
+
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':titulo', $titulo);
         $stmt->bindParam(':tipo', $tipo);
         $stmt->bindParam(':categoria', $categoria);
+        $stmt->bindParam(':diretor', $diretor);
+        $stmt->bindParam(':duracao', $duracao);
+        $stmt->bindParam(':titulo_original', $tituloOriginal);
+        $stmt->bindParam(':data_estreia', $dataEstreia);
+        $stmt->bindParam(':meta_dados', $metaDados);
+        $stmt->bindParam(':sinopse', $sinopse);
+        $stmt->bindParam(':pais_origem', $paisOrigem);
+        $stmt->bindParam(':roteiro', $roteiro);
+
+        
 
         if ($stmt->execute()) {
             echo "Dados inseridos com sucesso!";
